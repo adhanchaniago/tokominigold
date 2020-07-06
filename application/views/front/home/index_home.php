aa7082ff26e0b2c6390038ae7b6b54ef78cc323a
edi-prasetyo <ediprasetiyo2@gmail.com>
2020-07-06 15:32:48 +0700

    Update Slider

--- application/views/front/home/index_home.php
+++ application/views/front/home/index_home.php
@@ -3,8 +3,7 @@
   <ol class="carousel-indicators">
     <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
     <li data-target="#myCarousel" data-slide-to="1"></li>
-    <li data-target="#myCarousel" data-slide-to="2"></li>
-    <li data-target="#myCarousel" data-slide-to="3"></li>
+    
   </ol>
   <div class="carousel-inner">
     <?php $i=1; foreach ($slider as $slider) { ?>
